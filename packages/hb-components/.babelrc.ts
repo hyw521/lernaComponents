a11377882d833696cf43bc27bb02fbc7ef7cc440
yw <1499276590qq.com>
2021-11-15 19:36:59 +0800

    1

--- packages/hb-components/.babelrc.ts
+++ packages/hb-components/.babelrc.ts
@@ -1,15 +1,3 @@
 module.exports = {
   extends: require.resolve("../../babelrc.json"),
-  plugins: [
-    [
-      "import",
-      {
-        libraryName: "hbs-components",
-        customName: (name: string) => {
-          return `@hb-components/${name}`;
-        },
-      },
-      "hbs-components",
-    ],
-  ],
 };
